90214a28caa7fbe7ddfd2b92dbc84b8a669d4d15
Shawn Lee-Kwong <splitnova@gmail.com>
2016-03-24 12:36:26 -0400

    cloud recipes

--- cookbooks/postgresql9_extensions/recipes/ext_postgis2_install.rb
+++ cookbooks/postgresql9_extensions/recipes/ext_postgis2_install.rb
@@ -2,18 +2,21 @@ if @node[:postgres_version] >= 9.2
   proj_version = "4.6.1"
   geos_version = "3.4.2"
   python_exec_version = "0.2"
+  gdal_version = "1.10.0-r1"
+  postgis_version = "2.1.5"
 
-  if ey_release_version == '2009a'
-    gdal_version = "1.10.0"
-    postgis_version = "2.1.1-r1"
 
-    enable_package "dev-libs/json-c" do
-      version "0.11"
-    end
-  else
-    gdal_version = "1.10.0-r1"
-    postgis_version = "2.1.5"
-  end
+  # if ey_release_version == '2009a'
+  #   gdal_version = "1.10.0"
+  #   postgis_version = "2.1.1-r1"
+
+  #   enable_package "dev-libs/json-c" do
+  #     version "0.11"
+  #   end
+  # else
+  #   gdal_version = "1.10.0-r1"
+  #   postgis_version = "2.1.5"
+  # end
 
   package_use "sci-libs/geos" do
     flags "-ruby"
