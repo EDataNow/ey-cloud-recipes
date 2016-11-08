fd11b650be67112120567167821037ad8eda1231
Shawn Lee-Kwong <splitnova@gmail.com>
2016-11-08 14:21:01 -0500

    packages

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -9,4 +9,8 @@
 default[:packages] = [{:name => "media-libs/libpng", :version => "1.2.50"},
                       {:name => "media-fonts/arphicfonts", :version => "0.2.20080216.1"},
                       {:name => "media-libs/jpeg", :version => "6b-r9"},
-                      {:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.2.1"}]
+                      {:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.2.1"},
+                      {:name => "sci-libs/geos", :version => "3.4.2"},
+                      {:name => "sci-libs/proj", :version => "4.8.0"},
+                      {:name => "sci-libs/gdal", :version => "1.10.0-r1"},
+                      {:name => "sci-libs/libgeotiff", :version => "1.4.0"}]
